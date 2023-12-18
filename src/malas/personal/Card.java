fafe5fafe86da0e40d8032d9fe8139a1dbd1c151
Malas <csigerson2@huskers.unl.edu>
2023-12-17 18:52:55 -0600

    added card object

--- src/malas/personal/Card.java
+++ src/malas/personal/Card.java
@@ -1,9 +1,24 @@
 package malas.personal;
 
 public class Card {
+	
+	private final String suit;
+	private final int number;
 
-	public Card() {
-		// TODO Auto-generated constructor stub
+	public Card(String suit, int number) {
+		this.suit = suit;
+		this.number = number;
+		
+		if(!this.suit.equals("H") && !this.suit.equals("C") && !this.suit.equals("S") && !this.suit.equals("D")) {
+			throw new IllegalArgumentException("Invalid suit used in Card constructor");
+		} else if (this.number < 2 || this.number > 14) {
+			throw new IllegalArgumentException("Card number out of bounds");
+		}
 	}
 
+	public static void main(String[] args) {
+		Card fake = new Card("A", 1);
+	}
 }
+
+
